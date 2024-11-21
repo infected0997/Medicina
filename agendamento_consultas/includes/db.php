9880e41457abccc6d59a2d974dc90bd964f08a2c
infected0997 <72234291+infected0997@users.noreply.github.com>
2024-11-21 08:36:32 -0300

    Update db.php

--- agendamento_consultas/includes/db.php
+++ agendamento_consultas/includes/db.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "db"; 
-$username = "usuario";     
-$password = "senha";  
-$dbname = "saude_db";   
+$servername = "mysql";  // Nome do link do MySQL
+$username = "usuario";  // O nome de usuário definido no MySQL
+$password = "senha";    // A senha definida no MySQL
+$dbname = "meu_banco";  // O nome do banco de dados definido no MySQL
 
+// Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Checar a conexão
+// Verificar se a conexão foi bem-sucedida
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
+} else {
+    echo "Conectado ao banco de dados com sucesso!";
 }
-echo "Conectado com sucesso ao banco de dados!";
 ?>
